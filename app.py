0aa008f13c7951e4b55c7ca97c4a86c564f229c0
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-22 16:28:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,135 +1,62 @@
+from streamlit.components.v1 import html
 import pandas as pd
-import streamlit as st
-
-from streamlit_vizzu import Config, Data, Style, VizzuChart
-
-data_frame = pd.read_csv(
-    "https://ipyvizzu.vizzuhq.com/0.15/assets/data/infinite_data.csv",
-    dtype={"Year": str, "Timeseries": str},
-)
-data = Data()
-data.add_data_frame(data_frame)
-
-
-chart = VizzuChart(height=360)
-
-chart.animate(data)
-chart.feature("tooltip", True)
-
-chart.animate(
-
-    Config(
-
-        {
-
-            "channels": {
-
-                "x": ["Value 1", "Joy factors"],
-
-                "color": "Joy factors",
-
-                "label": "Value 1",
-
-            },
-
-            "title": "Pie Chart",
-
-            "coordSystem": "polar",
-
-        }
-
+from ipyvizzu import Chart, Data, Config, Style, DisplayTarget
+ 
+ 
+def create_chart():
+    # initialize Chart
+ 
+    chart = Chart(
+        width="640px", height="360px", display=DisplayTarget.MANUAL
     )
-
-)
-
-
-
-chart.animate(
-
-    Config(
-
-        {
-
-            "channels": {
-
-                "x": [
-
-                    "Value 1",
-
-                    "Joy factors",
-
-                    "Region",
-
-                    "Country code",
-
-                ],
-
-                "label": None,
-
+ 
+    # create and add data to Chart
+ 
+    data = Data()
+    data_frame = pd.read_csv(
+        "https://ipyvizzu.vizzuhq.com/0.15/showcases/titanic/titanic.csv"
+    )
+    data.add_data_frame(data_frame)
+ 
+    chart.animate(data)
+ 
+    # add config to Chart
+ 
+    chart.animate(
+        Config(
+            {
+                "x": "Count",
+                "y": "Sex",
+                "label": "Count",
+                "title": "Passengers of the Titanic",
             }
-
-        }
-
-    ),
-
-    duration="500ms",
-
-)
-
-
-
-chart.animate(
-
-    Config(
-
-        {
-
-            "channels": {
-
-                "x": [
-
-                    "Value 1",
-
-                    "Joy factors",
-
-                    "Region",
-
-                    "Country code",
-
-                ],
-
-                "y": {"set": "Value 3", "range": {"min": "-60%"}},
-
-            },
-
-            "title": "Coxcomb Chart",
-
-        }
-
+        )
     )
-
-)
-    
-output = chart.show()
-
-st.multiselect(
-    "Products",
-    ["Shoes", "Handbags", "Gloves", "Accessories"],
-    ["Shoes", "Handbags", "Gloves", "Accessories"],
-    key="items",
-)
-
-col1, col2, col3, col4, col5 = st.columns(5)
-
-measure: str = col1.radio(  # type: ignore
-    "Measure",
-    ["Sales", "Revenue [$]"],
-    key="measure",
-)
-compare_by = col2.radio("Compare by", ["Product", "Region", "Both"], key="compare_by")
-coords = col3.radio(
-    "Coordinate system", ["Cartesian (desktop)", "Polar (mobile)"], key="coords"
-)
-order = col4.radio("Order items", ["Alphabetically", "By value"], key="order")
-
-bg_color = col5.color_picker("Background color", "#fff", key="bg_color")
+    chart.animate(
+        Config(
+            {
+                "x": ["Count", "Survived"],
+                "label": ["Count", "Survived"],
+                "color": "Survived",
+            }
+        )
+    )
+    chart.animate(Config({"x": "Count", "y": ["Sex", "Survived"]}))
+ 
+    # add style to Chart
+ 
+    chart.animate(Style({"title": {"fontSize": 35}}))
+ 
+    # return generated html code
+ 
+    return chart._repr_html_()
+ 
+ 
+# generate Chart's html code
+ 
+CHART = create_chart()
+ 
+ 
+# display Chart
+ 
+html(CHART, width=650, height=370)
