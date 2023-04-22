9ddef0b905076dc6125dd79201ded498e2c2ab29
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-22 16:08:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,159 +1,83 @@
-# import streamlit, pandas and ipyvizzu
-from streamlit.components.v1 import html
-import streamlit as st 
-import pandas as pd
-from ipyvizzu import Chart, Data, Config, Style, DisplayTarget
- 
- 
 import pandas as pd
 import streamlit as st
 
-from streamlit_vizzu import Config, Data, VizzuChart
+from streamlit_vizzu import Config, Data, Style, VizzuChart
 
-
-@st.cache_data
-def get_data() -> pd.DataFrame:
-    return pd.read_csv(
-        "https://raw.githubusercontent.com/vizzuhq/vizzu-workshops/main/2022-11-11-PyData-NYC/data/trump_2020_05.csv"
-    )
-
-
+data_frame = pd.read_csv("pages/sales.csv")
 data = Data()
+data.add_data_frame(data_frame)
 
-data.add_data_frame(get_data())
+chart = VizzuChart(height=360)
 
-vchart = VizzuChart(height=360, return_clicks=False)
+chart.animate(data)
+chart.feature("tooltip", True)
 
-vchart.animate(data)
+bg_color = st.session_state.get("bg_color", "#fff")
+items = st.session_state.get("items", ["Shoes", "Handbags", "Gloves", "Accessories"])
+measure = st.session_state.get("measure", "Sales")
+compare_by = st.session_state.get("compare_by", "Region")
+coords = st.session_state.get("coords", "Cartesian")
+order = st.session_state.get("order", "By value")
 
-if "slide_num" not in st.session_state:
-    try:
-        st.session_state["slide_num"] = int(
-            st.experimental_get_query_params()["slide_num"][0]
-        )
-    except (KeyError, ValueError, IndexError):
-        st.session_state["slide_num"] = 0
+style = Style({"plot": {"backgroundColor": bg_color}})
 
-CHANNELS = {
-    "channels": {
-        "y": {
-            "set": ["tweets"],
-        },
-        "x": {"set": ["Period", "year", "month"]},
-        "color": "Period",
-    }
+filter = " || ".join([f"record['Product'] == '{item}'" for item in items])
+title = f"{measure} of " + ", ".join(items)
+
+if compare_by == "Product":
+    y = ["Product"]
+    x = [measure]
+    color = None
+
+elif compare_by == "Region":
+    y = [measure]
+    x = ["Region"]
+    color = ["Region"]
+
+else:
+    y = ["Product"]
+    x = [measure, "Region"]
+    color = ["Region"]
+
+config = {
+    "title": title,
+    "y": y,
+    "label": measure,
+    "x": x,
+    "color": color,
 }
 
+if coords == "Polar (mobile)":
+    config["coordSystem"] = "polar"
+else:
+    config["coordSystem"] = "cartesian"
 
-previous_slide = st.button(
-    "Previous slide", disabled=st.session_state["slide_num"] == 0
+if order == "Alphabetically":
+    config["sort"] = "none"
+else:
+    config["sort"] = "byValue"
+
+chart.animate(Data.filter(filter), Config(config), style, delay=0.1)
+output = chart.show()
+
+st.multiselect(
+    "Products",
+    ["Shoes", "Handbags", "Gloves", "Accessories"],
+    ["Shoes", "Handbags", "Gloves", "Accessories"],
+    key="items",
 )
-if previous_slide:
-    st.session_state["slide_num"] -= 1
 
-next_slide = st.button("Next slide", disabled=st.session_state["slide_num"] >= 9)
-if next_slide:
-    st.session_state["slide_num"] += 1
+col1, col2, col3, col4, col5 = st.columns(5)
 
-if st.session_state["slide_num"] == 0:
-    vchart.animate(
-        Data.filter("record.Firsttweet === 'Igen' && record.Dummy === 'Nem'"),
-        Config(
-            {
-                **CHANNELS,  # type: ignore
-                "title": "Trump started tweeting in May '09",
-            }
-        ),
-    )
-elif st.session_state["slide_num"] == 1:
-    vchart.animate(
-        Data.filter("record.Period === 'New to Twitter' && record.Dummy === 'Nem'"),
-        Config(
-            {
-                "title": "In the first two years he wasn't very active",
-                **CHANNELS,  # type: ignore
-            }
-        ),
-    )
-elif st.session_state["slide_num"] == 2:
-    vchart.animate(
-        Data.filter(
-            "(record.Period === 'New to Twitter' || record.Period === 'Businessman') "
-            "&& record.Dummy === 'Nem'"
-        ),
-        Config(
-            {
-                "title": "Then he got hooked on",
-                **CHANNELS,  # type: ignore
-            }
-        ),
-    )
+measure: str = col1.radio(  # type: ignore
+    "Measure",
+    ["Sales", "Revenue [$]"],
+    key="measure",
+)
+compare_by = col2.radio("Compare by", ["Product", "Region", "Both"], key="compare_by")
+coords = col3.radio(
+    "Coordinate system", ["Cartesian (desktop)", "Polar (mobile)"], key="coords"
+)
+order = col4.radio("Order items", ["Alphabetically", "By value"], key="order")
 
-elif st.session_state["slide_num"] == 3:
-    vchart.animate(
-        Data.filter(
-            "(record.Period === 'New to Twitter' || record.Period === 'Businessman' "
-            "|| record.Period === 'Nominee') && record.Dummy === 'Nem'"
-        ),
-        Config(
-            {
-                "title": "Interesting trend after becoming a presidential nominee",
-                **CHANNELS,  # type: ignore
-            }
-        ),
-    )
-
-elif st.session_state["slide_num"] == 4:
-    vchart.animate(
-        Data.filter("record.Dummy === 'Nem'"),
-        Config(
-            {
-                "title": "And after he became President",
-                **CHANNELS,  # type: ignore
-            }
-        ),
-    )
-
-elif st.session_state["slide_num"] == 5:
-    vchart.animate(
-        Config(
-            {
-                "geometry": "area",
-                "align": "center",
-                **CHANNELS,  # type: ignore
-            }
-        ),
-    )
-
-elif st.session_state["slide_num"] == 6:
-    vchart.animate(
-        Config(
-            {
-                "title": "All of Trump's tweets until May 2020",
-                **CHANNELS,  # type: ignore
-            }
-        ),
-    )
-
-elif st.session_state["slide_num"] == 7:
-    vchart.animate(
-        Config(
-            {
-                "y": "retweetcount",
-                "title": "And the number of times these were retweeted",
-            }
-        ),
-    )
-
-elif st.session_state["slide_num"] == 8:
-    vchart.animate(
-        Config(
-            {
-                "y": "tweets",
-                "title": "Let's focus on the number of tweets for now",
-            }
-        ),
-    )
-    st.write("Sorry, only did 8 slides for now")
-
-vchart.show()
+bg_color = col5.color_picker("Background color", "#fff", key="bg_color")
