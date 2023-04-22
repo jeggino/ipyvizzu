816a71b6eb26c56bd31afe4600fdc50edb0bc40c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-22 15:45:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,90 +6,81 @@ from ipyvizzu import Chart, Data, Config, Style, DisplayTarget
  
  
 data_frame = pd.read_csv(
-    "https://ipyvizzu.vizzuhq.com/0.15/showcases/music/music.csv",
-    dtype={"Year": str},
+    "https://ipyvizzu.vizzuhq.com/0.15/assets/data/chart_types_eu.csv",
+    dtype={"Year": str, "Timeseries": str},
 )
 data = Data()
 data.add_data_frame(data_frame)
  
 chart = Chart()
- 
 chart.animate(data)
- 
-chart.animate(
-    Config(
-        {
-            "x": "Year",
-            "y": ["Format", "Revenue [m$]"],
-            "color": "Format",
-            "geometry": "area",
-            "align": "center",
-            "title": "Music Revenue by Format 1973-2020",
-        }
-    ),
-    Style(
-        {
-            "plot": {
-                "xAxis": {"label": {"fontSize": 9, "angle": 2.0}},
-                "marker": {
-                    "colorPalette": "#b74c20FF #c47f58FF #1c9761FF"
-                    + " #ea4549FF #875792FF #3562b6FF"
-                    + " #ee7c34FF #efae3aFF"
-                },
-            }
-        }
-    ),
-)
- 
-chart.animate(
-    Config(
-        {
-            "align": "stretch",
-            "title": "Music Revenue by Format 1973-2020(%)",
-        }
-    ),
-    delay=1,
-)
- 
-chart.animate(
-    Config(
-        {
-            "align": "center",
-            "title": "Music Revenue by Format 1973-2020",
-        }
-    ),
-    delay=1,
-)
- 
-chart.animate(Config({"split": True}), delay=1)
- 
-chart.animate(
-    Data.filter(
-        "record.Format == 'Vinyl' ||record.Format == 'Streaming'"
-    ),
-    Config({"title": "Revenue of Vinyl & Streaming 1973-2020"}),
-    delay=1,
-)
- 
-chart.animate(
-    Data.filter(None),
-    Config(
-        {"title": "Music Revenue by Format 1973-2020", "split": False}
-    ),
-    delay=1,
-)
- 
-chart.animate(
-    Config(
-        {
-            "x": "Year",
-            "y": "Revenue [m$]",
-            "noop": "Format",
-            "align": "none",
-            "geometry": "line",
-        }
-    ),
-    delay=1,
-)
+
+	chart.animate(
+
+	    Config(
+
+	        {
+
+	            "channels": {
+
+	                "x": "Year",
+
+	                "y": ["Year", "Value 5 (+/-)"],
+
+	                "color": {
+
+	                    "set": ["Value 5 (+/-)"],
+
+	                    "range": {"min": "-45", "max": "45"},
+
+	                },
+
+	                "noop": "Country",
+
+	                "label": "Value 5 (+/-)",
+
+	            },
+
+	            "title": "Waterfall Chart",
+
+	            "legend": "color",
+
+	        }
+
+	    ),
+
+	    Style(
+
+	        {
+
+	            "plot": {
+
+	                "marker": {
+
+	                    "colorGradient": "#3d51b8 0,#6389ec 0.15,#9fbffa 0.35,#d5d7d9 0.5,#f4b096 0.65,#e36c56 0.85,#ac1727 1",
+
+	                    "label": {"position": "top"},
+
+	                }
+
+	            }
+
+	        }
+
+	    ),
+
+	)
+
+	 
+
+	chart.animate(
+
+	    Config(
+
+	        {"channels": {"y": "Value 5 (+/-)"}, "title": "Column Chart"}
+
+	    )
+
+	)
  
 html(chart._repr_html_())
