1cee44011e606bfae67e324cbad9a1e2c67eb7ea
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-04-23 09:09:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -108,6 +108,6 @@ import pandas_profiling
 from streamlit_pandas_profiling import st_profile_report
 
 df = pd.read_csv("https://storage.googleapis.com/tf-datasets/titanic/train.csv")
-pr = df.profile_report()
+pr = data_frame.profile_report()
 
 st_profile_report(data_frame)
